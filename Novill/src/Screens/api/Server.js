c488a579c30879cdca439886f87d214932b08ae4
lyaleaz <74859322+lyaleaz@users.noreply.github.com>
2022-12-15 19:40:36 +0200

    ....

--- Novill/src/Screens/api/Server.js
+++ Novill/src/Screens/api/Server.js
@@ -3,6 +3,6 @@ export default axios.create({
 
 
 
-    baseURL:'http://7368-2a10-8012-5-fc51-2847-b901-152e-4513.ngrok.io'
+    baseURL:'http://b3ff-2a10-8012-5-fc51-2847-b901-152e-4513.ngrok.io'
 
 });

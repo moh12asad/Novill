2fa5565af75bd54fecc662fed81d717cbac741dc
lyaleaz <74859322+lyaleaz@users.noreply.github.com>
2023-06-16 17:44:19 +0300

    design the home screens

--- Novill/src/Screens/DeliveryScreen.js
+++ Novill/src/Screens/DeliveryScreen.js
@@ -1,68 +1,56 @@
 import React from 'react';
-import {View,Button,StyleSheet,Text,ImageBackground} from 'react-native';
+import { View, Button, StyleSheet, Text, ImageBackground } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Spacer from './Components/Spacer';
 
+const DeliveryScreen = ({ navigation }) => {
+  return (
+    <ImageBackground
+      source={require("../Screens/images/background.jpg")}
+      style={{ width: '100%', height: '100%' }}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('SigninDelivery')}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Already have an account? Sign in here</Text>
+        </TouchableOpacity>
+        <Spacer />
+        <TouchableOpacity
+          onPress={() => navigation.navigate('SignupDelivery')}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Don't have an account? Sign Up here</Text>
+        </TouchableOpacity>
+        <Spacer />
+      </View>
+    </ImageBackground>
+  );
+};
 
-
-const DeliveryScreen=({navigation})=>{
-    return(
-    <ImageBackground source={require("../Screens/images/background.jpg")} style={{ width:'100%', height:'100%' }} >
-        <View style={{alignItems:'center',marginTop:300}}> 
-            <TouchableOpacity onPress={()=>navigation.navigate('SigninDelivery')}
-              style={{
-       backgroundColor: '#fff',
- 
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 170,
-
-
-    marginTop:10,
-    width:'80%',
-    borderColor: 'green',
-    alignItems:'center',
-     borderWidth: 2,
-        alignSelf:'flex-end',
-   
-    }}
-            >
-                    <Text style={{ color:'#000',fontSize:12,
-   fontWeight:"bold"}}>Already have an account? Sign in here</Text>
-            </TouchableOpacity>
-            <Spacer/>
-            <TouchableOpacity onPress={()=>navigation.navigate('SignupDelivery')}
-            style={{
-     
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginTop: 300,
+  },
+  button: {
     backgroundColor: '#fff',
- 
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 170,
-
-
-    marginTop:10,
-    width:'100%',
+    marginTop: 10,
+    width: '90%',
     borderColor: 'green',
-    alignItems:'center',
-     borderWidth: 2,
-        alignSelf:'flex-end',
-      
-    }}
-            
-            
-            >
-                <Text style={{ color:'#000',fontSize:12,
-   fontWeight:"bold"}} >Don't have account? Sign Up here</Text>
-            </TouchableOpacity>
-            <Spacer/>
-        </View>
-</ImageBackground>
-    );
-}
-
-const styles=StyleSheet.create({});
+    alignItems: 'center',
+    borderWidth: 2,
+    alignSelf: 'flex-end',
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+});
 
 export default DeliveryScreen;
-
-
